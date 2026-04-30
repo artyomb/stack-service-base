af56bd516e76f91ca30e708d7ed218fe8d912cbd
astrot1988 <astrot1988@gmail.com>
2026-04-30 19:41:11 +0300

    fix: remove socket debug

--- lib/stack-service-base/socket_trace.rb
+++ lib/stack-service-base/socket_trace.rb
@@ -2,11 +2,11 @@ module StackServiceBase
   module SocketTrace
     def bind(local_sockaddr)
       addr = Addrinfo.new(local_sockaddr)
-      LOGGER.info "Socket Bind: http://#{addr.ip_address}:#{addr.ip_port}"
+      # LOGGER.debug "Socket Bind: http://#{addr.ip_address}:#{addr.ip_port}"
       super
     end
     def setsockopt(level, optname, optval)
-      LOGGER.info "Socket setsockopt: #{level}, #{optname}, #{optval}"
+      # LOGGER.debug "Socket setsockopt: #{level}, #{optname}, #{optval}"
       super
     end
   end
